677eaf3073dbb885cf6932df611041065e73bf8f
Aluno SENAI <Aluno@SN-363479.edu.fiesc.com.br>
2026-09-15 15:29:13 -0300

    feat/controller para atividades no banco

--- backend/controller/Atividade.controller.js
+++ backend/controller/Atividade.controller.js
@@ -1,22 +1,45 @@
-import { Atividade } from "../model/Atividade.model";
+import { Atividade } from "../model/Atividade.model.js";
 
-export const controlletAtividade = {
+export const controllerAtividade = {
   criar: async (req, res) => {
     try {
+      const atividade = req.body;
 
-        const atividade = req.body
-
-        const response = await Atividade.create(atividade)
-
-
-        
+      const response = await Atividade.create(atividade);
     } catch (err) {
-        console.error(err.message)
+      console.error(err.message);
 
-        res.status(501).json({
-            error: err,
-            success: false
-        })
+      res.status(501).json({
+        error: err,
+        success: false,
+      });
+    }
+  },
+  buscarTodasPaginadas: async (req, res) => {
+    
+    const params = req.params
+
+    const page = Number(params.page)
+    const limit = Number(params.limit)
+
+    const offset = (page - 1) * limit
+
+    try {
+
+      const response = await Atividade.findAll({ offset: offset, limit: limit})
+
+      
+      res.status(200).json({
+        success: true,
+        data: response
+      })
+    } catch (err) {
+      console.error(err.message);
+
+      res.status(501).json({
+        error: err,
+        success: false,
+      });
     }
   },
 };
