88dd73c71a1ffaa405d57e4a04d0dde79393de18
RENATO FRASSA FERREIRA DA SILVA <renato_ff_silva@estudante.sesisenai.org.br>
2026-09-16 17:50:53 -0300

    importação do componente de criar atividade

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import { Route, Router, Routes } from 'react-router-dom'
 import Home from './tabs/Home'
+import CriarAtividade from './tabs/CriarAtividade'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/criaratividade' element={<CriarAtividade></CriarAtividade>}></Route>
     </Routes>
   )
 }
